7607ad365ace7030635d8e74f8b2a76fb641ca5c
Stephane <stephane@pluxml.org>
2015-05-13 14:54:05 +0200

    [+] Compatibilité PluXml 5.4

--- config.php
+++ config.php
@@ -15,23 +15,30 @@ if(!empty($_POST)) {
 }
 
 ?>
-
-<h2><?php echo $plxPlugin->getInfo('title') ?></h2>
-<p><?php $plxPlugin->lang('L_DESCRIPTION'); ?></p>
-
-<form action="parametres_plugin.php?p=plxMyGoogleAnalytics" method="post">
+<style>
+form.inline-form label {
+	width: 250px;
+}
+</style>
+<form class="inline-form" action="parametres_plugin.php?p=plxMyGoogleAnalytics" method="post">
 	<fieldset>
-		<p class="field"><label for="accountId"><?php $plxPlugin->lang('L_ACCOUNT_ID'); ?> : </label></p>
-		<input type="text" name="accountId" value="<?php echo plxUtils::strCheck($plxPlugin->getParam('accountId')) ?>" />
-		&nbsp;ex:&nbsp;XX-XXXXXXXX-X
-		<p class="field"><label for="websiteUrl"><?php $plxPlugin->lang('L_WEBSITE_URL'); ?> : </label></p>
-		<input type="text" name="websiteUrl" value="<?php echo plxUtils::strCheck($plxPlugin->getParam('websiteUrl')) ?>" />
-		&nbsp;ex:&nbsp;mywebsite.com
-		<p class="field"><label for="bounceRate"><?php $plxPlugin->lang('L_BOUNCE_RATE'); ?> : </label></p>
-		<input type="text" name="bounceRate" value="<?php echo plxUtils::strCheck($plxPlugin->getParam('bounceRate')) ?>" />
-		&nbsp;<?php $plxPlugin->lang('L_BOUNCE_RATE_HELP1'); ?>
-		<a class="help" title="<?php $plxPlugin->lang('L_BOUNCE_RATE_HELP2'); ?>">&nbsp;</a>
 		<p>
+			<label for="accountId"><?php $plxPlugin->lang('L_ACCOUNT_ID'); ?> : </label>
+			<input type="text" name="accountId" value="<?php echo plxUtils::strCheck($plxPlugin->getParam('accountId')) ?>" />
+			&nbsp;ex:&nbsp;XX-XXXXXXXX-X
+		</p>
+		<p>
+			<label for="websiteUrl"><?php $plxPlugin->lang('L_WEBSITE_URL'); ?> : </label>
+			<input type="text" name="websiteUrl" value="<?php echo plxUtils::strCheck($plxPlugin->getParam('websiteUrl')) ?>" />
+			&nbsp;ex:&nbsp;mywebsite.com
+		</p>
+		<p>
+			<label for="bounceRate"><?php $plxPlugin->lang('L_BOUNCE_RATE'); ?> : </label>
+			<input type="text" name="bounceRate" value="<?php echo plxUtils::strCheck($plxPlugin->getParam('bounceRate')) ?>" />
+			&nbsp;<?php $plxPlugin->lang('L_BOUNCE_RATE_HELP1'); ?>
+			<a class="help" title="<?php $plxPlugin->lang('L_BOUNCE_RATE_HELP2'); ?>">&nbsp;</a>
+		</p>
+		<p class="in-action-bar">
 			<?php echo plxToken::getTokenPostMethod() ?>
 			<input type="submit" name="submit" value="<?php $plxPlugin->lang('L_SAVE'); ?>" />
 		</p>
