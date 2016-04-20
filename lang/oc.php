ce09e2b000ac0880092c3e9952fcc1a865a9597c
Quent-in <pages_quentin@hotmail.com>
2016-04-20 19:41:15 +0200

    Occitan version

---
+++ lang/oc.php
@@ -0,0 +1,12 @@
+<?php
+$LANG = array(
+'L_TITLE'				=> 'MyGoogleAnalytics',
+'L_DESCRIPTION'			=> 'Analisi d\'audiéncia Internet',
+'L_ACCOUNT_ID'			=> 'Identificant del compte Google Analytics',
+'L_WEBSITE_URL'			=> 'URL del site sens http:// nimai www.',
+'L_BOUNCE_RATE'	  		=> 'Taus de rebomb en segonda<br />Valor conselhat : 15 segondas',
+'L_BOUNCE_RATE_HELP1'	=> 'Metre la valr a 0 per desactivar aqueste paramètre',
+'L_BOUNCE_RATE_HELP2'	=> 'Se lo nombre de segondas es passat, la visita de la pagina non serà pas presa en compte dins lo taus de rebomb.',
+'L_SAVE'				=> 'Enregistrar'
+);
+?>
